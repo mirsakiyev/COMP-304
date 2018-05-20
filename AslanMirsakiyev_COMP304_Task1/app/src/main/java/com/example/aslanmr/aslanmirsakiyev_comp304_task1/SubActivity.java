d4d122800d35101f4e614a6b92f971123df1824d
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-05-19 23:51:57 -0400

    Assigment1 - Task1 - Added appropriate methods and functionality, final design changes and Added comments

--- AslanMirsakiyev_COMP304_Task1/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_task1/SubActivity.java
+++ AslanMirsakiyev_COMP304_Task1/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_task1/SubActivity.java
@@ -1,7 +1,12 @@
 package com.example.aslanmr.aslanmirsakiyev_comp304_task1;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        //Initialize Bundle
+        Bundle bundle=getIntent().getExtras();
+
+        //Create strings to contain text values from myName and my Hobby
+        String myName=bundle.getString("myName");
+        String myHobby=bundle.getString("myHobby");
+
+        TextView myNameTextView=findViewById(R.id.nameTextView_Sub);
+        TextView myHobbyTextView=findViewById(R.id.hobbyTextView_Sub);
+
+        //Set text values to TextViews(myName, myHobby)
+        myNameTextView.setText("Name is "+myName);
+        myHobbyTextView.setText("Hobby is "+myHobby);
     }
 }
