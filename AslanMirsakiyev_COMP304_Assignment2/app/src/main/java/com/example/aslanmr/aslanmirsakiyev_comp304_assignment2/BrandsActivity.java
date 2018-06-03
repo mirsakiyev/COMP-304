5ea89ca5bee41744e28bad0e5a23f92675fd0050
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-03 18:38:29 -0400

    Created a BrandsActivity, added functionality so when you select an item from list view on CategoriesActivity it takes you to BrandsActivity and stores user choice in temp variable

---
+++ AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/BrandsActivity.java
@@ -0,0 +1,22 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BrandsActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brands);
+
+        textView=findViewById(R.id.categoryTextView);
+
+        String TempHolder=getIntent().getStringExtra("ListViewClickValue");
+
+        textView.setText(TempHolder);
+    }
+}
--- AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/CategoriesActivity.java
+++ AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/CategoriesActivity.java
@@ -1,13 +1,47 @@
 package com.example.aslanmr.aslanmirsakiyev_comp304_assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CategoriesActivity extends AppCompatActivity {
+
+
+    ListView listView;
+
+    String[] categoriesItems=new String[]{
+                                            "Phones",
+                                            "Laptops",
+                                            "Smart Watches",
+                                            "TVs"
+                                    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        listView=findViewById(R.id.categoriesListView);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_2, android.R.id.text1, categoriesItems);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String TempListView=categoriesItems[position].toString();
+
+                Intent intent=new Intent(CategoriesActivity.this, BrandsActivity.class);
+
+                intent.putExtra("ListViewClickValue", TempListView);
+
+                startActivity(intent);
+            }
+        });
     }
 }
