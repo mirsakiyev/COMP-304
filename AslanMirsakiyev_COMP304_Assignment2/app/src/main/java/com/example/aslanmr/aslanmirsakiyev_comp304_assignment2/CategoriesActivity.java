b5edd21886b09242e347e340224e5a20259f0a1f
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-03 17:34:13 -0400

    Assignment2 - Created a CategoriesActivity and added a shopBtn to InitialActivity which takes user to CategoriesActivity

---
+++ AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/CategoriesActivity.java
@@ -0,0 +1,13 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
--- AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/InitialActivity.java
+++ AslanMirsakiyev_COMP304_Assignment2/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment2/InitialActivity.java
@@ -1,7 +1,9 @@
 package com.example.aslanmr.aslanmirsakiyev_comp304_assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+    }
+
+    public void shopBtn_Click(View v)
+    {
+        if (v.getId()==R.id.shopBtn)
+        {
+            Intent intent=new Intent(InitialActivity.this, CategoriesActivity.class);
+
+            startActivity(intent);
+        }
     }
 }
