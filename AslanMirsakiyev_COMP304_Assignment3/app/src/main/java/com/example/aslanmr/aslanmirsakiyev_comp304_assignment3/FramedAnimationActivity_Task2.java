d1f785ea56c5267e983a7d020b936206371a7b54
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-17 18:49:15 -0400

    Create FramedAnimationActivity and TweenedAnimationActivity for tasks 2 and 3

---
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/FramedAnimationActivity_Task2.java
@@ -0,0 +1,13 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FramedAnimationActivity_Task2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_framed_animation__task2);
+    }
+}
---
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/TweenedAnimationActivity_Task3.java
@@ -0,0 +1,13 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TweenedAnimationActivity_Task3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweened_animation__task3);
+    }
+}
