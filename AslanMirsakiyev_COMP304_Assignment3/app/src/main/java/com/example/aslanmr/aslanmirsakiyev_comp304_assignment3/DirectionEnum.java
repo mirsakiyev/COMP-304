3ffb04841c927ba38b0edd6cb1c915ed8b387425
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-17 18:46:43 -0400

    Created a DrawingActivity_Task1 and enum java class for Direction

---
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/DirectionEnum.java
@@ -0,0 +1,8 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment3;
+
+public enum DirectionEnum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
---
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/DrawingActivity_Task1.java
@@ -0,0 +1,13 @@
+package com.example.aslanmr.aslanmirsakiyev_comp304_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawingActivity_Task1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing__task1);
+    }
+}
