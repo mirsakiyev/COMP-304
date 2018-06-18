450cc96fd6672e649d1a1fa728615b264dbb4854
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-18 17:39:22 -0400

    Created all nececcary methods for task2/ TODO: make those methods work

--- AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/FramedAnimationActivity_Task2.java
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/FramedAnimationActivity_Task2.java
@@ -6,14 +6,65 @@ package com.example.aslanmr.aslanmirsakiyev_comp304_assignment3;
  * Assignment 3 - Framed Animation - Task 2
  */
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FramedAnimationActivity_Task2 extends AppCompatActivity {
+
+    AnimationDrawable mframeAnimation=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_framed_animation__task2);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        final Button btnStart=findViewById(R.id.task2_btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
+        final Button btnStop=findViewById(R.id.task2_btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAnimation();
+            }
+        });
+    }
+
+    private void startAnimation()
+    {
+
+    }
+
+    private void stopAnimation()
+    {
+        mframeAnimation.stop();
+        mframeAnimation.setVisible(false, false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            
+            case android.R.id.home;
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
