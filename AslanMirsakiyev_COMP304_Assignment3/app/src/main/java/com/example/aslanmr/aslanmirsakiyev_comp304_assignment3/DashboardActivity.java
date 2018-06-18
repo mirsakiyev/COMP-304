e754bc7a40e59d79803f88909b78fba4d4d422fe
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-17 20:08:18 -0400

    Finished design of task1, all major elements are in place

--- AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/DashboardActivity.java
+++ AslanMirsakiyev_COMP304_Assignment3/app/src/main/java/com/example/aslanmr/aslanmirsakiyev_comp304_assignment3/DashboardActivity.java
@@ -10,7 +10,6 @@ import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
